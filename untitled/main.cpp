16ac7f50815d6c361e6cee4fb102aecc44ac4863
IgorDiasV <igorverissimo02@hotmail.com>
2019-09-06 14:53:45 -0300

    att

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
-void putBox(int x0, int x1, int y0, int y1, int z0, int z1)
+void putVoxel(int x,int y,int z)
 {
-    cout<<"OFF\n8 6 0"<<endl;
-    cout<<x0<<" "<<y0<<" "<<z0<<endl;
-    cout<<x0<<" "<<y1<<" "<<z0<<endl;
-    cout<<x1<<" "<<y1<<" "<<z0<<endl;
-    cout<<x1<<" "<<y0<<" "<<z0<<endl;
-    cout<<x0<<" "<<y0<<" "<<z1<<endl;
-    cout<<x0<<" "<<y1<<" "<<z1<<endl;
-    cout<<x1<<" "<<y1<<" "<<z1<<endl;
-    cout<<x1<<" "<<y0<<" "<<z1<<endl;
-    cout<<"4 0 3 2 1 1 1 1 1\n4 4 5 6 7 1 1 1 1\n4 0 1 5 4 1 1 1 1\n4 0 4 7 3 1 1 1 1\n4 3 7 6 2 1 1 1 1\n4 1 2 6 5 1 1 1 1"<<endl;
+    ofstream fout;
+    fout.open("/home/igor/Documentos/arquivo.off");
+    if(!fout.is_open())
+    {
+        exit(0);
+    }
+    fout<<"OFF\n8 6 0"<<endl;
+    fout<<x-0.5<<" "<<y+0.5<<" "<<z-0.5<<endl;
+    fout<<x-0.5<<" "<<y-0.5<<" "<<z-0.5<<endl;
+    fout<<x+0.5<<" "<<y-0.5<<" "<<z-0.5<<endl;
+    fout<<x+0.5<<" "<<y+0.5<<" "<<z-0.5<<endl;
+    fout<<x-0.5<<" "<<y+0.5<<" "<<z+0.5<<endl;
+    fout<<x-0.5<<" "<<y-0.5<<" "<<z+0.5<<endl;
+    fout<<x+0.5<<" "<<y-0.5<<" "<<z+0.5<<endl;
+    fout<<x+0.5<<" "<<y+0.5<<" "<<z+0.5<<endl;
+    fout<<"4 0 3 2 1 1 1 1 1\n4 4 5 6 7 1 1 1 1\n4 0 1 5 4 1 1 1 1\n4 0 4 7 3 1 1 1 1\n4 3 7 6 2 1 1 1 1\n4 1 2 6 5 1 1 1 1"<<endl;
 
-
+    fout.close();
 
 
 }
 int main()
 {
 
-    putBox(0,2,0,3,0,5);
+    putVoxel(2,5,5);
     return 0;
 }
