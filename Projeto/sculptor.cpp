c11b92802cfa98073e9f9a740acf3f69002e6019
IgorDiasV <igorverissimo02@hotmail.com>
2019-09-11 18:57:56 -0300

    att

--- Projeto/sculptor.cpp
+++ Projeto/sculptor.cpp
@@ -1,8 +1,26 @@
 #include "sculptor.h"
-
+Voxel ***x;
 Sculptor::Sculptor(int _nx, int _ny, int _nz)
 {
+    //np=nz,nl=nx,nc=ny
+    x=new Voxel**[_nz*_nx*_ny];
 
+    for(int i = 0; i < _nz; i++){
+        x[i] = new Voxel*[_nx*_ny];
+    }
+    for(int i = 0; i < _nz; i++){
+        for(int j = 0; j <_nx; j++){
+            x[i][j] = new Voxel[_ny];
+        }
+    }
+
+}
+
+Sculptor::~Sculptor()
+{
+
+    delete [] *x;
+    delete [] **x;
 }
 
 void Sculptor::setColor(float r, float g, float b, float alpha)
--- Projeto/sculptor.h
+++ Projeto/sculptor.h
@@ -1,11 +1,12 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
-
+#include <string>
+using namespace std;
 struct Voxel {
-  float r,g,b; // Colors
-  float a;
-// Transparency
-  bool isOn; // Included or not
+    float r,g,b; // Colors
+    float a;
+    // Transparency
+    bool isOn; // Included or not
 };
 
 class Sculptor
@@ -17,7 +18,18 @@ protected:
     float r,g,b,a; // Current drawing color
 public:
     Sculptor(int _nx, int _ny, int _nz);
+    ~Sculptor();
     void setColor(float r, float g, float b, float alpha);
+    void putVoxel(int x, int y, int z);//n
+    void cutVoxel(int x, int y, int z);//n
+    void putBox(int x0, int x1, int y0, int y1, int z0, int z1);//n
+    void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);//n
+    void putSphere(int xcenter, int ycenter, int zcenter, int radius);//n
+    void cutSphere(int xcenter, int ycenter, int zcenter, int radius);//n
+    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);//n
+    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);//n
+    void writeOFF(string filename);//n
+    void writeVECT(string filename);//n
 };
 
 #endif // SCULPTOR_H
