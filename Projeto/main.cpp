f4fc49e354dc281fd5543d0177006aae7febd49c
IgorDiasV <igorverissimo02@hotmail.com>
2019-09-12 08:16:08 -0300

    att

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-
+    // [altura][profundidade][largura]
     Sculptor p(2,2,2);
-    p.putVoxel(1,0,0);
+    p.putVoxel(1,0,0);//vermelho
     p.setColor(0,0,1,1);
-    p.putVoxel(0,1,0);
+    p.putVoxel(0,1,0);//azul
     p.setColor(0,1,0,1);
-    p.putVoxel(0,0,1);
+    p.putVoxel(0,0,1);//verde
+    p.setColor(1,1,1,1);
+    p.putVoxel(0,0,0);
+
 
     p.writeOFF("arquivo.off");
     system("geomview arquivo.off");
