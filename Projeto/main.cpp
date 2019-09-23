43ad4bb391c14fd198417327d8a6bbb7601ff8fd
IgorDiasV <igorverissimo02@hotmail.com>
2019-09-22 22:12:51 -0300

    att

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -6,19 +6,17 @@ using namespace std;
 int main()
 {
     // [altura][profundidade][largura]
+    // z x y
     Sculptor p(30,30,30);
-    p.setColor(color(254),color(225),color(45),1);
-   /* for(int i=0;i<50;i++)
-    {
-        p.putEllipsoid(5,5,i,5,5,1);
-        p.putEllipsoid(5,20,i,5,5,1);
-        p.putEllipsoid(5,35,i,5,5,1);
-        p.putEllipsoid(30,5,i,5,5,1);
-        p.putEllipsoid(30,20,i,5,5,1);
-        p.putEllipsoid(30,35,i,5,5,1);
-    }*/
-    p.putSphere(10,10,10,10);
-
+    p.putBox(0,20,0,20,0,20);
+    p.cutBox(0,10,0,10,0,10);
+    p.setColor(0,0,1,1);
+    p.putSphere(5,5,5,5);
+    p.cutVoxel(19,19,19);
+    p.putVoxel(19,19,19);
+    p.cutEllipsoid(15,15,15,6,5,4);
+    p.setColor(0,1,0,1);
+    p.putEllipsoid(15,15,15,6,5,4);
     p.writeOFF("arquivo.off");
     system("meshlab arquivo.off");
 
