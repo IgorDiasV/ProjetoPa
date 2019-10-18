9e0a69f7299d32991bfd841cea720ad7a9c7ac42
IgorDiasV <igorverissimo02@hotmail.com>
2019-10-17 21:16:17 -0300

    att

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -11,21 +11,34 @@ int main()
 {
     // [altura][profundidade][largura]
     // z x y
-   /* Sculptor p(20,20,20);
+        /*p.putBox(0,20,0,20,0,20);
+        p.cutBox(0,10,0,10,0,10);
+        p.setColor(0,0,1,1);
+        p.putSphere(5,5,5,5);
+        p.cutVoxel(19,19,19);
+        p.putVoxel(19,19,19);
+        p.cutEllipsoid(15,15,15,6,5,4);
+        p.setColor(0,1,0,1);
+        p.putEllipsoid(15,15,15,6,5,4);*/
+    Sculptor p(30,30,30);
     vector<FiguraGeometrica*> fig;
+    fig.push_back(new PutBox(0, 20, 0, 20, 0, 20,0,1,0,1));
+    fig.push_back(new CutBox(0, 10, 0, 10, 0, 10));
+    fig.push_back(new PutSphere(5, 5, 5, 5, 1, 1, 1,1));
+    fig.push_back(new CutVoxel(19, 19, 19));
+    fig.push_back(new PutVoxel(19,19,19,1,1,1,1));
+    fig.push_back(new PutVoxel(20,20,20,1,1,1,1));
 
-    fig.push_back(new PutVoxel(1, 1, 1, 1, 0, 0, 1));
-    fig.push_back(new PutVoxel(2, 2, 2, 0, 1, 0, 1));
-    fig.push_back(new PutVoxel(3, 3, 3, 0, 0, 1, 1));
-    fig.push_back(new PutSphere(19, 19, 19, 5, 1, 1, 1, 1));
+    fig.push_back(new CutEllipsoid(15,15,15,6,5,4));
+    fig.push_back(new PutEllipsoid(15,15,15,8,5,6,1,1,1,1));
 
     for(unsigned int i=0; i<fig.size(); i++){
         fig[i]->draw(p);
     }
 
-    p.writeOFF("alo.off");
-    system("meshlab alo.off");*/
-    AbrirProjeto s("teste.txt");
+    p.writeOFF("figura.off");
+    system("meshlab figura.off");
+   // AbrirProjeto s("fig.txt");
 
     return 0;
 }
